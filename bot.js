2f449343ee8017d4e8e4e8fc55013df269a81e3d
denverku <magicph26@gmail.com>
2023-01-26 19:05:47 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,9 +164,24 @@ const startBot = async () => {
      try { 
          await bot.launch() 
          console.log('Bot started successfully') 
+         console.log(getIPAddress());
      } catch(error) { 
          console.error(error) 
      } 
  } 
   
  startBot()
+
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+  return '0.0.0.0';
+}
